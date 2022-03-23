656f514ae816235e4ff0f1022d41c21c91787830
Alejandro Rebaldería <ale.rebal@gmail.com>
2022-03-23 09:39:29 +0000

    fix app url

--- codestart/settings.py
+++ codestart/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ale-codestart-2022.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['ale-codestart-2022.herokuapp.com', 'localhost']
 
 
 # Application definition
