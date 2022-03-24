0e0f941fe51e0770161ce8833f3b49fe5c97453c
Alejandro Rebaldería <ale.rebal@gmail.com>
2022-03-24 11:17:08 +0000

    Add templates and css

--- blog/models.py
+++ blog/models.py
@@ -42,7 +42,7 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    appoved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_on']
