79e463b3bc78debce1cb15a89b4ed053aeee0e7f
Alejandro Rebaldería <ale.rebal@gmail.com>
2022-03-25 11:48:33 +0000

    Add comments section

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
-
+    """comment"""
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
@@ -12,8 +13,9 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
-
+    """comment"""
     def get(self, request, slug, *args, **kwargs):
+        """comment"""
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
@@ -27,7 +29,44 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
             },            
         )
 
+    def post(self, request, slug, *args, **kwargs):
+        """comment"""
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            # post['wrong_comment'] = False
+        else:
+            comment_form = CommentForm()
+            # post['wrong_comment'] = True
+            
+        
+        return render(
+            request,
+            'post_detail.html',
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                # "wrong_comment" : post.wrong_comment,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },      
+        )
