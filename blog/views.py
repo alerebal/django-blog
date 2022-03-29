7f3265a2ff850ce469864a34c269f932fa2395ff
Alejandro Rebaldería <ale.rebal@gmail.com>
2022-03-29 08:06:40 +0000

    Add PostLike view and messages styles

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -52,10 +53,8 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
-            # post['wrong_comment'] = False
         else:
             comment_form = CommentForm()
-            # post['wrong_comment'] = True
             
         
         return render(
@@ -65,8 +64,21 @@ class PostDetail(View):
                 "post": post,
                 "comments": comments,
                 "commented": True,
-                # "wrong_comment" : post.wrong_comment,
                 "liked": liked,
                 "comment_form": CommentForm()
             },      
         )
+
+
+class PostLike(View):
+    """commetn"""
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=self.request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        
